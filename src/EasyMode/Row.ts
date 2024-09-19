184b1b59c31e5cf64a73230e97c702a83d3c7b57
penghui <penghui.zhang@foxmail.com>
2024-09-19 22:51:30 +0800

    feat: layout easy mode

---
+++ src/EasyMode/Row.ts
@@ -0,0 +1,26 @@
+import { LayoutObject } from './LayoutObject'
+
+export class Row extends LayoutObject {
+  public children: LayoutObject[]
+
+  constructor(...children: LayoutObject[]) {
+    super()
+    this.children = children
+  }
+
+  getWidth() {
+    let result = 0
+    for (const child of this.children) {
+      result += child.getWidth()
+    }
+    return result
+  }
+
+  getHeight() {
+    let result = 0
+    for (const child of this.children) {
+      result = Math.max(result, child.getHeight())
+    }
+    return result
+  }
+}
